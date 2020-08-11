b09e1a1276339172598f27feacb59d46391407e2
github username <github email address>
2020-08-11 08:59:55 -0700

    react router working perfectly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,33 @@
 import React from "react";
 import "../App.css";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
         <img src="WALL.png" alt="WALL Y" width="150"></img>
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">Projects</Button>
-        <Button color="inherit">Resume</Button>
-        <Button color="inherit">Contact</Button>
+        <Button color="inherit">
+          <NavLink to="/" id="link">
+            Home
+          </NavLink>
+        </Button>
+        <Button color="inherit">
+          <NavLink to="/projects" id="link">
+            Projects
+          </NavLink>
+        </Button>
+        <Button color="inherit">
+          <NavLink to="/resume" id="link">
+            Resume
+          </NavLink>
+        </Button>
+        <Button color="inherit">
+          <NavLink to="/contact" id="link">
+            Contact
+          </NavLink>
+        </Button>
       </Toolbar>
     </AppBar>
   );
